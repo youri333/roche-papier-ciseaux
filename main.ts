b8f905f5bcf20612de15ca3cf767df2067d2c6fc
youri333 <83732831+youri333@users.noreply.github.com>
2021-05-08 15:08:02 -0400

    Mettre à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -26,6 +26,19 @@ radio.onReceivedNumber(function (receivedNumber) {
             . . . . .
             `)
     }
+    y = randint(0, 2)
+    basic.pause(2000)
+    if (receivedNumber == 2 && y == 0) {
+        basic.showString("V")
+    } else if (receivedNumber == 1 && y == 2) {
+        basic.showString("V")
+    } else if (receivedNumber == 0 && y == 1) {
+        basic.showString("V")
+    } else if (receivedNumber == y) {
+        basic.showString("E")
+    } else {
+        basic.showString("D")
+    }
 })
 input.onGesture(Gesture.Shake, function () {
     radio.sendNumber(y)
