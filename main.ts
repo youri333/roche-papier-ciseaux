a26547a3e051d31cbb184905e386591ea573491c
youri333 <83732831+youri333@users.noreply.github.com>
2021-05-08 13:10:21 -0400

    Mettre à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
 let y = 0
+radio.onReceivedNumber(function (receivedNumber) {
+    radio.sendValue("name", 6)
+    radio.sendNumber(6)
+})
 input.onGesture(Gesture.Shake, function () {
     y = randint(0, 2)
     if (y == 0) {
