d72202734211432af26c23b2cb4b4a6cabdf2e5a
youri333 <83732831+youri333@users.noreply.github.com>
2021-05-08 13:46:18 -0400

    Mettre à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,31 @@
 let y = 0
 radio.onReceivedNumber(function (receivedNumber) {
-    radio.sendNumber(y)
+    y = randint(0, 2)
+    if (y == 0) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (y == 1) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
 })
 input.onGesture(Gesture.Shake, function () {
     radio.sendNumber(y)
